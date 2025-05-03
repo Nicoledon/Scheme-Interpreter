5a886b86427bfd23099fec968f1b467870829d11
Nicoledon <3121506708@qq.com>
2025-05-03 18:52:44 +0800

    lab09

--- lab09/lab09.scm
+++ lab09/lab09.scm
@@ -1,10 +1,27 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2) 
+  (cond
+   ((> num1 num2) 1)
+   ((= num1 num2) 0)
+   ((< num1 num2) (- 0 1))    
+  ) 
+)
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (make-adder num)
+  (lambda (inc) (+ inc num))
+)
 
-(define (composed f g) 'YOUR-CODE-HERE)
-
-(define (repeat f n) 'YOUR-CODE-HERE)
+(define (composed f g) 
+    (lambda (x) (f (g x)))
+)
+(define (repeat_helper f n count)
+  (if 
+    (= count 1) (f n )
+    (f (repeat_helper f n (- count 1))) 
+  ) 
+)
+(define (repeat f n) 
+    (lambda (num) (repeat_helper f num n ))
+)
 
 (define (max a b)
   (if (> a b)
@@ -16,4 +33,11 @@
       b
       a))
 
-(define (gcd a b) 'YOUR-CODE-HERE)
+(define (gcd a b) 
+  (cond
+   ((zero? a) b)
+   ((zero? b) a)
+   ((= (modulo (max a b) (min a b)) 0) (min a b))
+   (else (gcd (max a b) (modulo (max a b) (min a b))))
+  )
+)
